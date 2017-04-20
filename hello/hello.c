10bc5040c11e296947eaaa6d8f781858b9f96df5
Katleho Khanye <kakhanye@yahoo.com>
2017-04-20 22:28:24 +0200

    first commit

---
+++ hello/hello.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int index;
+	
+	index = 0;
+	while (str[index] != '\0')
+	{
+		ft_putchar(str[index]);
+		index++;
+	}
+}
+
+int	main(void)
+{
+	ft_putstr("Hello World!\n");
+	return (0);	
+}
